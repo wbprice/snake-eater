5d9ee6bb6e666153d71ac10ab9a532b497f1a693
Blaine Price <1wbprice@gmail.com>
2019-05-20 23:03:55 -0400

    Placeholder sprite moves with WASD

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ use amethyst::renderer::{DisplayConfig, DrawFlat2D, Pipeline,
                          RenderBundle, Stage};
 use amethyst::utils::application_root_dir;
 use amethyst::core::transform::TransformBundle;
+use amethyst::input::InputBundle;
 
 mod snake_eater;
+mod systems;
 
 use crate::snake_eater::SnakeEater;
 
@@ -13,6 +15,14 @@ fn main() -> amethyst::Result<()> {
 
     let path = format!("{}/resources/display_config.ron", application_root_dir());
     let config = DisplayConfig::load(&path);
+
+    let binding_path = format!(
+            "{}/resources/bindings_config.ron",
+            application_root_dir()
+        );
+
+        let input_bundle = InputBundle::<String, String>::new()
+            .with_bindings_from_file(binding_path)?;
 
     let pipe = Pipeline::build()
         .with_stage(
@@ -26,7 +36,10 @@ fn main() -> amethyst::Result<()> {
         RenderBundle::new(pipe, Some(config))
             .with_sprite_sheet_processor()
         )?
-        .with_bundle(TransformBundle::new())?;
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(input_bundle)?
+        .with(systems::BigBossSystem, "big_boss_system", &["input_system"]);
+
 
     let mut game = Application::new("./", SnakeEater, game_data)?;
 
--- src/snake_eater.rs
+++ src/snake_eater.rs
@@ -9,8 +9,8 @@ use amethyst::renderer::{
 
 pub const ARENA_HEIGHT: f32 = 480.0;
 pub const ARENA_WIDTH: f32 = 480.0;
-const BIG_BOSS_HEIGHT: f32 = 32.0;
-const BIG_BOSS_WIDTH: f32 = 64.0;
+pub const BIG_BOSS_HEIGHT: f32 = 32.0;
+pub const BIG_BOSS_WIDTH: f32 = 64.0;
 
 pub struct SnakeEater;
 
