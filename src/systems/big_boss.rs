aedb01ab4cdb829dea9d8c3e87bff04e8ef28dd4
Blaine Price <1wbprice@gmail.com>
2019-05-23 13:52:01 -0400

    This is approximately working, but I feel like I don't understand how entity origin points work.

--- src/systems/big_boss.rs
+++ src/systems/big_boss.rs
@@ -3,7 +3,7 @@ use amethyst::ecs::{Join, Read, ReadStorage, System, WriteStorage};
 use amethyst::input::InputHandler;
 
 // You'll have to mark PADDLE_HEIGHT as public in snake eater.rs
-use crate::snake_eater::{BigBoss, ARENA_HEIGHT, BIG_BOSS_HEIGHT};
+use crate::snake_eater::{BigBoss, ARENA_HEIGHT, ARENA_WIDTH};
 
 pub struct BigBossSystem;
 
@@ -15,18 +15,59 @@ impl<'s> System<'s> for BigBossSystem {
     );
 
     fn run(&mut self, (mut transforms, big_bosses, input): Self::SystemData) {
-        for (_big_boss, transform) in (&big_bosses, &mut transforms).join() {
+        for (big_boss, transform) in (&big_bosses, &mut transforms).join() {
             let x_mov = input.axis_value("horizontal");
             let y_mov = input.axis_value("vertical");
 
+            let big_boss_x = transform.translation().x;
+            let big_boss_y = transform.translation().y;
+
+            dbg!(big_boss_x);
+            dbg!(big_boss_y);
+
+            // // Prevent player from walking off the top of the screen
+            // if big_boss_y >= ARENA_HEIGHT - big_boss.height / 2.0 && y_mov > 0.0 {
+            //     continue;
+            // }
+
+            // // Prevent player from walking off the bottom of the screen
+            // if big_boss_y < big_boss.height / 2.0 && y_mov < 0.0 {
+            //     continue;
+            // }
+
+            // // Prevent player from walking off the left or right of the screen
+            // if big_boss_x >= ARENA_WIDTH - big_boss.width / 2.0 || big_boss_x < big_boss.width / 2.0 {
+            //     continue;
+            // }
+
             if let Some(mv_amount) = x_mov {
                 let scaled_amount = 2.0 * mv_amount as f32;
-                transform.translate_x(scaled_amount);
+                // Prevent the player from walking off the left and right of the screen.
+                if scaled_amount > 0.0 {
+                    if big_boss_x <= ARENA_WIDTH - big_boss.width / 2.0 {
+                        transform.translate_x(scaled_amount);
+                    }
+                }
+                else if scaled_amount < 0.0 {
+                    if big_boss_x >= 0.0 + big_boss.width / 2.0 {
+                        transform.translate_x(scaled_amount);
+                    }
+                }
             }
 
             if let Some(mv_amount) = y_mov {
                 let scaled_amount = 2.0 * mv_amount as f32;
-                transform.translate_y(scaled_amount);
+                // Prevent the player from walking off the top or the bottom of the screen.
+                if scaled_amount > 0.0 {
+                    if big_boss_y <= ARENA_HEIGHT - big_boss.height {
+                        transform.translate_y(scaled_amount);
+                    }
+                }
+                else if scaled_amount < 0.0 {
+                    if big_boss_y > 0.0 + big_boss.height {
+                        transform.translate_y(scaled_amount);
+                    }
+                }
             }
         }
     }
